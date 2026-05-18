a870459976eb04d446b721e73965661642ac682b
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-05-18 15:59:17 +0900

    chore: init project

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('서버 실행 중');
+});
