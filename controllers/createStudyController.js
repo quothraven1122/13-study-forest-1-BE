4082015ed35ec0007ef276a45e9c62e0eaf88a13
HoneyLatlll <rlaalstn9415@naver.com>
2026-05-23 22:38:07 +0900

    fix: #9 스터디 생성 API 에러 처리 개선

--- controllers/createStudyController.js
+++ controllers/createStudyController.js
@@ -4,6 +4,12 @@ export const createStudy = async (req, res) => {
   try {
     const { name, description, password, nickname, background } = req.body;
 
+    if (!name || !description || !password || !nickname || !background) {
+      return res
+        .status(400)
+        .json({ message: '필수 가입 요구사항 입력 데이터 누락' });
+    }
+
     const newStudy = await prisma.study.create({
       data: { name, description, password, nickname, background },
     });
@@ -12,7 +18,7 @@ export const createStudy = async (req, res) => {
       ...newStudy,
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(500).json({
       message: err.message,
     });
   }
