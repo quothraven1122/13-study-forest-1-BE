942de046279f8d8d04079133acae283516e1a184
sgwag6799 <sgwag6799@gmail.com>
2026-05-26 15:45:04 +0900

    feat: pointController.js 구현, index.js import 추가, server.js 라우트 설정 완료

--- controllers/pointController.js
+++ controllers/pointController.js
@@ -1,1 +1,37 @@
 // 오늘의 집중 페이지의 point PATCH API
+import prisma from '../prisma/index.js';
+
+// GET - name이랑 point 가져오기
+export const getStudyFocus = async (req, res) => {
+  const { studyId } = req.params;
+
+  const study = await prisma.study.findUnique({
+    where: { id: Number(studyId) },
+    select: {
+      name: true,
+      point: true,
+    },
+  });
+
+  return res.status(200).json(study);
+};
+
+// PATCH - 포인트 수정
+export const patchStudyPoint = async (req, res) => {
+  const { studyId } = req.params;
+  const { points } = req.body;
+
+  const study = await prisma.study.update({
+    where: { id: Number(studyId) },
+    data: {
+      point: {
+        increment: points,
+      },
+    },
+    select: {
+      point: true,
+    },
+  });
+
+  return res.status(200).json(study);
+};
