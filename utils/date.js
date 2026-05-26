b320e0d5812610d5767b141a4eea872526544199
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-05-26 13:31:03 +0900

    fix: #12 change getStartAndEndOfWeek so that sundays do not return next week monday

--- utils/date.js
+++ utils/date.js
@@ -1,8 +1,11 @@
 export const getStartAndEndOfWeek = () => {
   const now = new Date();
 
+  const day = now.getDay(); // 일:0 ~ 토:6
+  const diff = day === 0 ? -6 : 1 - day; //일요일(0)이면 -6해서 이번주 월요일로 가져오기
+
   const startOfWeek = new Date(now);
-  startOfWeek.setDate(now.getDate() - now.getDay() + 1); // 월요일 기준
+  startOfWeek.setDate(now.getDate() + diff); // 월요일 기준
   startOfWeek.setHours(0, 0, 0, 0);
 
   const endOfWeek = new Date(startOfWeek);
