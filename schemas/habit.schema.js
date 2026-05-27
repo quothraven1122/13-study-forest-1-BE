2d84eaad6b072b8248e37810c7778552e749ee20
apple <apple@appleui-MacBookAir.local>
2026-05-27 13:34:49 +0900

    feat: 유효성 검사 수정완료

--- schemas/habit.schema.js
+++ schemas/habit.schema.js
@@ -1,8 +1,10 @@
 import z from 'zod';
-
-export const createHabitSchema = z.object({
+/// 습관(등록, 수정) 유효성 검사
+export const habitSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(1, '습관은 1글자 이상이어야 합니다')
     .max(20, '습관은 20글자 이하여야 합니다'),
 });
+
--- schemas/study.schema.js
+++ schemas/study.schema.js
@@ -1,34 +1,40 @@
 import z from 'zod';
-
+/// 스터디 생성 유효성 검사
 export const createStudySchema = z.object({
   name: z
     .string()
+    .trim()
     .min(1, '스터디 이름은 1글자 이상이어야 합니다')
     .max(20, '스터디 이름은 20글자 이하여야 합니다'),
   description: z
     .string()
+    .trim()
     .min(1, '소개는 1글자 이상이어야 합니다')
     .max(100, '소개 100글자 이하여야 합니다'),
   password: z.string().min(6, '비밀번호는 6글자 이상이어야 합니다'),
   nickname: z
     .string()
+    .trim()
     .min(1, '닉네임은 1글자 이상이어야 합니다')
     .max(20, '닉네임은 20글자 이하여야 합니다'),
 });
-
+/// 스터디 수정 유효성 검사
 export const updateStudySchema = z.object({
   name: z
     .string()
+    .trim()
     .min(1, '스터디 이름은 1글자 이상이어야 합니다')
     .max(20, '스터디 이름은 20글자 이하여야 합니다')
     .optional(),
   description: z
     .string()
+    .trim()
     .min(1, '소개는 1글자 이상이어야 합니다')
     .max(100, '소개 100글자 이하여야 합니다')
     .optional(),
   nickname: z
     .string()
+    .trim()
     .min(1, '닉네임은 1글자 이상이어야 합니다')
     .max(20, '닉네임은 20글자 이하여야 합니다')
     .optional(),
