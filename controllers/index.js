022eef03b0e785b4158d8d4d6b874750f32feb2c
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-05-29 15:45:42 +0900

    fix: #72 add deleted import

--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,6 @@ export * from './createStudyController.js';
 export * from './studyGetController.js';
 export * from './pointController.js';
 export * from './emojiController.js';
+export * from './deleteStudyController.js';
 
 export { createHabit, updateHabit, deleteHabit } from './habitController.js';
