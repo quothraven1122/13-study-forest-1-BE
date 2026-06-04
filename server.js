a15c38c09c38271fa0cad2f44a9da7acd2ee30fd
seonghwi-ha <digh7515@gmail.com>
2026-06-04 10:08:00 +0900

    머지 충돌 해결

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(express.json());
 
 app.get('/studies', studyController.getAllStudy);
 app.post('/studies', studyController.createStudy);
-// 전체 스터디
-app.get('/studies', getAllStudy);
-app.post('/studies', createStudy);
 
 app.get('/studies/:studyId', studyController.getStudyDetail);
 app.patch('/studies/:studyId', studyController.updateStudy);
@@ -33,23 +30,23 @@ app.delete('/studies/:studyId/habits/:habitId', habitController.deleteHabit);
 app.get(`/studies/:studyId/focus`, studyController.getStudyFocus);
 app.patch(`/studies/:studyId/focus`, studyController.patchStudyPoint);
 // habits 관련 라우터
-app.get('/studies/:studyId/habits', getHabits);
-app.post('/studies/:studyId/habits', createHabit);
-app.patch('/studies/:studyId/habits/:habitId', updateHabit);
-app.delete('/studies/:studyId/habits/:habitId', deleteHabit);
+app.get('/studies/:studyId/habits', habitController.getHabits);
+app.post('/studies/:studyId/habits', habitController.createHabit);
+app.patch('/studies/:studyId/habits/:habitId', habitController.updateHabit);
+app.delete('/studies/:studyId/habits/:habitId', habitController.deleteHabit);
 
 // focus 관련 라우터
-app.get('/studies/:studyId/focus', getStudyFocus);
-app.patch('/studies/:studyId/focus', patchStudyPoint);
+app.get('/studies/:studyId/focus', studyController.getStudyFocus);
+app.patch('/studies/:studyId/focus', studyController.patchStudyPoint);
 
 // 기타 study 하위 라우터
-app.post('/studies/:studyId/confirm-pw', postPwCheck);
-app.post('/studies/:studyId/emoji', createEmoji);
+app.post('/studies/:studyId/confirm-pw', studyController.postPwCheck);
+app.post('/studies/:studyId/emoji', studyController.createEmoji);
 
 // 스터디 상세/수정/삭제
-app.get('/studies/:studyId', getStudyDetail);
-app.patch('/studies/:studyId', updateStudy);
-app.delete('/studies/:studyId', deleteStudy);
+app.get('/studies/:studyId', studyController.getStudyDetail);
+app.patch('/studies/:studyId', studyController.updateStudy);
+app.delete('/studies/:studyId', studyController.deleteStudy);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('서버 실행 중');
